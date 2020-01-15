26834c685013286f71f76eae26e3c4d1e42d193d
Robert Eustace Novak <sailnfool@gmail.com>
2020-01-15 15:42:09 -0800

    Added a script to convert a number into H:m:s format

---
+++ scripts/func.hmsout.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+####################
+# hmsout <number> <units>
+#   Convert the number in hours, minutes or seconds into H:m:s format
+####################
+# Author - Robert E. Novak aka REN
+#	sailnfool@gmail.com
+#	skype:sailnfool.ren
+#_____________________________________________________________________
+# Rev.|Auth.| Date     | Notes
+#_____________________________________________________________________
+# 1.0 | REN |01/15/2020| original version
+#_____________________________________________________________________
+####################
+source func.errecho
+if [ -z "${__func_hmsout}" ]
+then
+	export __func_hmsout=1
+	function hmsout() {
+		number=$1
+    units=$2
+    case ${units} in
+      seconds)
+        ((hour=${number}/3600))
+        ((mins=(${number}-hour*3600)/60))
+        ((sec=${number}=((hour*3600) + (mins*60))))
+        ;;
+      minutes)
+        ((hour=${number}/60))
+        ((mins=(${number}-hour*60)))
+        ((sec=0))
+        ;;
+      hours)
+        ((hour=${number}))
+        ((mins=0))
+        ((sec=0))
+        ;;
+      \?)
+        errecho ${LINENO} ${FUNCNAME} "Invalid units=${units}"
+        exit 1
+         ;;
+    esac
+    print "%02d:%02d:%02d" "${hour}" "${$mins}" "${sec}"
+  }
+fi # if [ -z "${__func_hmsout}" ]
+# vim: set syntax=bash, ts=2, sw=2, lines=55, columns=120,colorcolumn=78
