0eb46e2ebdcc0d808f30d8fec88e2364d33df7bc
Robert E. Novak <sailnfool@gmail.com>
2021-03-01 13:51:57 -0800

    New version

--- scripts/func.debug.sh
+++ scripts/func.debug.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+####################
+# Copyright (c) 2021 Sea2Cloud
+# 3901 Moorea Dr.
+# Modesto, CA 95356
+# 408-910-9134
+#
+# Author - Robert E. Novak aka REN
+#	sailnfool@gmail.com
+#	skype:sailnfool.ren
+#
+#_____________________________________________________________________
+# Rev.|Auth.| Date     | Notes
+#_____________________________________________________________________
+# 1.1 | REN |03/01/2021| Added DEBUGOFF
+# 1.0 | REN |07/22/2010| Initial Release
+#_____________________________________________________________________
+#
+
 ########################################################################
 # Author: Robert E. Novak
-# email: novak5llnl.gov, sailnfool@gmail.com
+# email: sailnfool@gmail.com
 #
 # Set up global definitions of debugging levels
 ########################################################################
@@ -9,15 +27,18 @@
 # set the debug level to zero
 # Define the debug levels:
 #
-# DEBUGSTRACE - prefix the executable with strace (if implemented)
-# DEBUGSETX - turn on set -x to debug
-# DEBUGNOEXECUTE - generate and display the command lines but don't
+# DEBUGOFF	0
+# DEBUGSTRACE	5 - prefix the executable with strace (if implemented)
+# DEBUGNOEXECUTE	or
+# DEBUGNOEX	6 - generate and display the command lines but don't
 #                  execute the benchmark
+# DEBUGSETX	9 - turn on set -x to debug
 ####################
 if [ -z "${__funcdebug}" ]
 then
   export DEBUGOFF=0
-	export DEBUGSTRACE=5
+  export DEBUGSTRACE=5
   export DEBUGNOEXECUTE=6
+  export DEBUGNOEX=6
   export DEBUGSETX=9
 fi # if [ -z "${__funcdebug}" ]
