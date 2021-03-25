fc8abb480d6be4311adf408b97722baa21331fce
Robert E. Novak <sailnfool@gmail.com>
2021-03-25 05:44:50 -0700

    Changed usage string

--- scripts/mypr.sh
+++ scripts/mypr.sh
@@ -20,14 +20,14 @@
 source func.errecho
 
 
-USAGE="\n${0##*/} [-[hd]] <file> ...\n
-\t\tprint file(s) with proper options set\n
-\t-h\tPrint this message\n
-\t-c\tturn on color syntax highlighting\n
-\t-d\tturn on diagnostics for this script\n
-\t-e\tuse enscript\n
-\t-p\t<printer>\tName of the printer (default Canon_MX920_series)\n
-\t-l\t<command>\tCommand for printer (lp|lpr) (lpr is default)\n
+USAGE="\r\n${0##*/} [-[hd]] <file> ...\r\n
+\t\tprint file(s) with proper options set\r\n
+\t-h\tPrint this message\r\n
+\t-c\tturn on color syntax highlighting\r\n
+\t-d\tturn on diagnostics for this script\r\n
+\t-e\tuse enscript\r\n
+\t-p\t<printer>\tName of the printer (default Canon_MX920_series)\r\n
+\t-l\t<command>\tCommand for printer (lp|lpr) (lpr is default)\r\n
 "
 
 optionargs="hcdel:p:"
