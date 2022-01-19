8c4e274edc05fee37e6b0cd10c31ecf7475209fc
Robert E. Novak <sailnfool@gmail.com>
2022-01-19 05:46:58 -0800

    Moved locker and logger out of ren

---
+++ scripts/func.locker.sh
@@ -0,0 +1,246 @@
+#!bin/bash
+########################################################################
+# Author: Robert E. Novak
+# email: sailnfool@gmail.com
+# Copyright (C) 2021 Sea2Cloud Storage, Inc. All Rights Reserved
+# Modesto, CA 95356
+#
+# Set up Global variables for testing scripts
+# Define func_getlock and func_release to avoid conflicts
+#
+########################################################################
+#_____________________________________________________________________
+# Rev.|Auth.| Date     | Notes
+#_____________________________________________________________________
+# 1.1 | REN |03/25/2021| added getcounter putcounter getnextcounter
+# 1.0 | REN |03/15/2021| original version
+#_____________________________________________________________________
+#
+if [ -z "${__func_locker}" ]
+then
+	export __func_locker=1
+	
+	source func.errecho
+  source func.regex
+
+  ####################
+  # Directories that must be created by func_setuplocks
+  ####################
+	export HOME_RESULTS=${HOME_RESULTS:-${HOME}/.results}
+	export TESTDIR=${TESTDIR:-${HOME_RESULTS}/testdir}
+	export ETCDIR=${ETCDIR:-${TESTDIR}/etc}
+
+  ####################
+  # Files that must be created by func_setuplocks
+  ####################
+	export LOCKERRS=${ETCDIR}/lockerrs
+	export TESTLOG=${HOME_RESULTS}/testlog.txt
+	export LOCKFILE_TESTNUMBERFILE=${HOME_RESULTS}/TESTNUMBER.lock
+	export LOCKFILE_BATCHNUMBERFILE=${HOME_RESULTS}/BATCHNUMBER.lock
+
+  ####################
+  # The maximum number of spins allowed waiting for a lock
+  ####################
+	export MAXSPINS=10
+  export SPINWAIT=20
+
+	####################
+	# updating the following requires locking
+	####################
+	export TESTNUMBERFILE=${HOME_RESULTS}/TESTNUMBER
+	export BATCHNUMBERFILE=${HOME_RESULTS}/BATCHNUMBER
+	####################
+	# End of files that need locking
+	####################
+
+  ############################################################
+	# func_getlock lockfile sleeptime maxspins
+  ############################################################
+	func_getlock()
+	{
+		lockcount=0
+		sleeptime=1
+		maxspins="${MAXSPINS}"
+		OLD_FUNC_VERBOSE=${FUNC_VERBOSE}
+		FUNC_VERBOSE=1
+
+		re='^[0-9]+$'
+		maxspins=${MAXSPINS}
+    case $# in
+      0)
+  			errecho "${FUNCNAME}" "${LINENO}" \
+  			    "No lockfile specified"
+  			exit 1
+        ;;
+      1)
+			  lockfile="$1"
+        ;;
+      2)
+			  lockfile="$1"
+				if ! [[ "$2" =~ $re_integer ]]; then
+					errecho "${FUNCNAME}" "${LINENO}" \
+						"Not a number - $2"
+					exit 1
+				fi
+				sleeptime="$2"
+        ;;
+      3)
+			  lockfile="$1"
+				if ! [[ "$2" =~ $re_integer ]]; then
+					errecho "${FUNCNAME}" "${LINENO}" \
+						"Not a number - $2"
+					exit 1
+				fi
+				sleeptime="$2"
+				if ! [[ "$3" =~ $re_integer ]]; then
+					errecho "${FUNCNAME}" "${LINENO}" \
+						"Not a number - $3"
+					exit 1
+				fi
+				maxspins="$3"
+        ;;
+      \?)
+        errecho "${FUNCNAME}" "${LINENO}" \
+          "Invalied arguments $@"
+        ;;
+    esac
+
+		while [ -f "${lockfile}" ]
+		do
+			errecho "${FUNCNAME}" "${LINENO}" \
+				"Sleeping on lock acquisition for lock owned by"
+			errecho "${FUNCNAME}" "${LINENO}" \
+				$(ls -l "${lockfile}")
+			((++lockcount))
+			if [ "${lockcount}" -gt "${maxspins}" ]
+			then
+				errecho "${FUNCNAME}" "${LINENO}" \
+					"Exceeded ${maxspins} spins waiting for lock, quitting"
+				exit 1
+			fi
+			sleep ${sleeptime}
+		done
+		FUNC_VERBOSE="${OLD_FUNC_VERBOSE}"
+		touch "${lockfile}"
+
+	}
+	export -f func_getlock
+
+	################################################################
+	# func_releaselock lockfile
+	################################################################
+	func_releaselock()
+	{
+		if [ $# -eq 0 ]
+		then
+			errecho "${FUNCNAME}" "${LINENO}" \
+			    "No lockfile specified"
+			exit 1
+#		else
+#			lockfile="$1"
+#			if [ -r "${lockfile}" ]
+#			then
+#				errecho "${FUNCNAME}" "${LINENO}" \
+#					"no such file as ${lockfile}"
+#			fi
+		fi
+    lockfile="$1"
+		rm -f "${lockfile}"
+	}
+	export -f func_releaselock
+
+  ############################################################
+  # func_setuplocks
+  ############################################################
+  func_setuplocks()
+  {
+    mkdir -p ${HOME_RESULTS} ${TESTDIR} ${ETCDIR} 
+    touch ${LOCKERRS} ${TESTLOG}
+
+    if [ ! -r ${BATCHNUMBERFILE} ]
+    then
+      func_getlock ${LOCKFILE_BATCHNUMBERFILE}
+      echo 0 > ${BATCHNUMBERFILE}
+      func_releaselock ${LOCKFILE_BATCHNUMBERFILE}
+    fi
+    if [ ! -r ${TESTNUMBERFILE} ]
+    then
+      func_getlock ${LOCKFILE_TESTNUMBERFILE}
+      echo 0 > ${TESTNUMBERFILE}
+      func_releaselock ${LOCKFILE_TESTNUMBERFILE}
+    fi
+  }
+  export -f func_setuplocks
+
+	################################################################
+	# Get a batch number for batch of tests
+	################################################################
+	func_getbatchnumber()
+	{
+    value=$(func_getnextcounter "${LOCKFILE_BATCHNUMBERFIL}" \
+      "${BATCHNUMBERFILE}")
+		echo "${value}"
+	}
+	export -f func_getbatchnumber
+
+	################################################################
+	# Get a test number for a test
+	################################################################
+	func_gettestnumber()
+	{
+    value=$(func_getnextcounter "${LOCKFILE_TESTNUMBERFILE}" \
+      "${TESTNUMBERFILE}")
+		echo "${value}"
+	}
+	export -f func_gettestnumber
+
+  ############################################################
+  # get a counter value
+  ############################################################
+  func_getcounter()
+  {
+    if [ $# -ne 2 ]
+    then
+      insufficient 2
+    fi
+    func_getlock "$1"
+    if [ ! -r "$2" ]
+    then
+      echo 0 > "$2"
+    fi
+    value=$(cat "$2")
+    func_releaselock "$1"
+    echo "${value}"
+  }
+  export -f func_getcounter
+
+  ############################################################
+  # put a counter value
+  ############################################################
+  func_putcounter()
+  {
+    if [ $# -ne 3 ]
+    then
+      insufficient 3
+    fi
+    func_getlock "$1"
+    echo "$3" > "$2"
+    func_release "$1"
+  }
+  export -f func_putcounter
+
+  ############################################################
+  # get the next counter value
+  ############################################################
+  func_getnextcounter()
+  {
+    if [ $# -ne 2 ]
+    then
+      insufficient 2
+    fi
+    value=$(func_getcounter "$1" "$2")
+    ((value++))
+    func_putcounter "$1" "$2" "${value}"
+  }
+  export -f func_getnextcounter
+fi #if [ -z "${__func_locker}" ]
---
+++ scripts/func.logger.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+#######################################################################
+# Author: Robert E. Novak
+# email: sailnfool@gmail.com
+# Copyright (C)2021 Sea2Cloud Storage, Inc.  All Rights Reserved
+# Modesto, CA 95356
+#
+# Log entries in a common log file for tracking down problems
+# with scripts going astray.
+#
+#######################################################################
+# Log entries willbe single line records and will  contain multiple
+# fields and the fields will be separated by a vertical pipe
+# character "|" to separate the fields of the record.
+#
+# EVENT|execname|process#|BATCH#|Test#|TimeSTAMP|Event|Specific|Fields
+#
+# EVENT Types:
+# START Record for when the test begins
+# FINISH Record for when the test ends
+# DELTA Record that records information about the wall time of the 
+#     Test - about the interval between START and FINISH
+# RATE Record that tracks information about the rate at which the
+#     processes	execute to insure that we are able to
+#     schedule/allocate adequate time	for future runs of
+#     the application
+#######################################################################
+# Rev.|Auth.| Date     | Notes
+#_____________________________________________________________________
+# 1.0 | REN |02/22/2021| Initial Release
+#_____________________________________________________________________
+#######################################################################
+
+
+if [ -z "${__funclogger}" ]
+then
+	export __funclogger=1
+
+	source func.errecho
+	source func.insufficient
+
+  function logger()
+  {
+    if [ $# -gt 0 ]
+    then
+      echo -n "$1" >> ${TESTLOG}
+    fi
+    shift
+    while [ $# -gt 0 ]
+    do
+      echo -n "|$1" >> ${TESTLOG}
+      shift
+    done
+    echo "" >> ${TESTLOG}
+  }
+	export -f logger
+	function old_logger()
+	{
+
+		####################
+		# Since the test number is a global variable stored in the file
+		# system, it is passed in.  Every 20 tests, output the titles
+		# for the records in the log file
+		####################
+		# errecho ${FUNCNAME} ${LINENO} "#5=$5"
+		if [[ ! $5 =~ -*[0-9]+ ]]
+		then
+			mod_test_number="999999"
+		else
+			mod_test_number=$(expr $5 % 20)
+		fi
+		# errecho ${FUNCNAME} ${LINENO} "#mod_test_number=$mod_test_number"
+		if [ ${mod_test_number} -eq 0 ]
+		then
+			####################
+			# Initialize the titles
+			####################
+			commontitles="${commontitles}Executable|"
+			commontitles="${commontitles}Process #|"
+			commontitles="${commontitles}Batch #|"
+			commontitles="${commontitles}Test #|"
+			
+			starttitles="START|${commontitles}Filesystem|"
+			starttitles="${starttitles}Start Time|"
+			starttitles="${starttitles}Num Processes|"
+			starttitles="${starttitles}Num Nodes|"
+	
+			finishtitles="FINISH|${commontitles}Filesystem|"
+			finishtitles="${finishtitles}Finish Time|"
+			finishtitles="${finishtitles}Num Processes|"
+			finishtitles="${finishtitles}Num Nodes|"
+	
+			deltatitles="DELTA|${commontitles}Filesystem|"
+			deltatitles="${deltatitles}Duration Time|"
+			deltatitles="${deltatitles}Duration Seconds|"
+			deltatitles="${deltatitles}Num Processes|"
+	
+			ratetitles="RATE|${commontitles}Filesystem|"
+			ratetitles="${ratetitles}Duration Time|"
+			ratetitles="${ratetitles}Num Processes|"
+			ratetitles="${ratetitles}Rate Band|"
+			ratetitles="${ratetitles}Processes Per Hour|"
+		fi
+
+		####################
+		# Check the initial arguments
+		####################
+		NUMARGS=5
+		if [ $# -lt ${NUMARGS} ]
+		then
+			insufficient ${FUNCNAME} ${LINENO} ${NUMARGS} $@
+		fi
+		logevent="$1"
+		logexec="$2"
+		# logexec="$2"
+		# logprocessnumber="$3"
+		# logbatch="$4"
+		# logtestnum="$5"
+
+		logexecbase=${logexec##*/}
+		upper_exec=$(echo ${logexecbase}|tr [:lower:] [:upper:])
+		if [ -z "${TESTLOG}" ]
+		then
+			if [ -z "${TESTDIR}" ]
+			then
+				errecho ${FUNCNAME} ${LINENO} \
+					"Environment variable TESTDIR not set"
+				errecho ${FUNCNAME} ${LINENO} \
+					"Environment variable TESTLOG not set"
+				exit 1
+			else
+				export ETCDIR=${TESTDIR}/etc
+				export TESTLOG=${ETCDIR}/testlog.txt
+			fi
+		fi
+
+    		case ${logevent} in
+			START | FORK | SLEEPING | COMPLETED)
+				# Handle the START Log entry
+				####################
+				# Write the titles to the log files
+				####################
+				if [ ${mod_test_number} -eq 0 ]
+				then
+					echo "${starttitles}" >> ${TESTLOG}
+				fi
+				NUMSTARTARGS=9
+				if [ $# -lt ${NUMSTARTARGS} ]
+				then
+					errecho ${FUNCNAME} ${LINENO} \
+						"error in ${logevent} parameter count"
+					insufficient ${FUNCNAME} ${LINENO} ${NUMSTARTARGS} $@
+				fi
+# 				logsourcedir="$6"
+#				logdestdir="$7"
+# 				logdate_began="$8"
+# 				lognumprocs="$9"
+				if [ -z "${TESTLOG}" ]
+				then
+					errecho ${FUNCNAME} ${LINENO} \
+					    "TESTLOG not initialized"
+					exit 1
+				fi
+				echo \
+					"${1}|${2}|${3}|${4}|${5}|${6}|${7}|${8}|${9}" >> ${TESTLOG}
+				;;
+      NOTE)
+        if [ ${mod_test_number} -eq 0 ]
+        then
+          echo "${starttitles}" >> ${TESTLOG}
+        fi
+        NUMNOTEARGS=7
+        if [ $# -lt ${NUMNOTEARGS} ]
+        then
+          errecho ${FUNCNAME} ${LINENO} \
+            "error in ${logevent} parameter count"
+          insufficient ${FUNCNAME} ${LINENO} ${NUMNOTEARGS} $@
+        fi
+				if [ -z "${TESTLOG}" ]
+				then
+					errecho ${FUNCNAME} ${LINENO} \
+					    "TESTLOG not initialized"
+					exit 1
+				fi
+				echo \
+					"${1}|${2}|${3}|${4}|${5}|${6}|${7}" >> ${TESTLOG}
+				;;
+			FINISH)	# Handle the FINISH Log entry
+				if [ ${mod_test_number} -eq 0 ]
+				then
+
+				####################
+				# Write the titles to the log files
+				####################
+					echo "${finishtitles}" >> ${TESTLOG}
+				fi
+				NUMFINISHARGS=9
+				if [ $# -lt ${NUMFINISHARGS} ]
+				then
+					errecho ${FUNCNAME} ${LINENO} \
+						"error in ${logevent} parameter count"
+					insufficient ${FUNCNAME} ${LINENO} ${NUMFINISHARGS} $@
+				fi
+# 				logsourcedir="$6"
+#				logdestdir="$7"
+# 				logdate_began="$8"
+# 				lognumprocs="$9"
+				echo \
+					"${1}|${2}|${3}|${4}|${5}|${6}|${7}|${8}|${9}" \
+					>> ${TESTLOG}
+				;;
+			DELTA)	# Handle the DELTA Log entry
+				if [ ${mod_test_number} -eq 0 ]
+				then
+	
+					####################
+					# Write the titles to the log files
+					####################
+					echo "${deltatitles}" >> ${TESTLOG}
+				fi
+				NUMDELTAARGS=11
+				if [ $# -lt ${NUMDELTAARGS} ]
+				then
+					errecho ${FUNCNAME} ${LINENO} \
+						"error in ${logevent} parameter count"
+					insufficient ${FUNCNAME} ${LINENO} ${NUMDELTAARGS} $@
+				fi
+# 				logsourcedir="$6"
+#				logdestdir="$7"
+# 				logdate_began="$8"
+# 				lognumprocs="$9"
+				echo \
+					"${1}|${2}|${3}|${4}|${5}|${6}|${7}|${8}|${9}" \
+					>> ${TESTLOG}
+				;;
+			\?)
+				errecho ${FUNCNAME} ${LINENO} "Invalid Log Type ${logevent}"
+				exit 1
+				;;
+    esac
+	}
+	export -f old_logger
+fi # if [ -z "${__funclogger}" ]
