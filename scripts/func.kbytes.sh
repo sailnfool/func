7c5343a006e3d65db88ee2fe0afcdc656aaae8b5
Robert E. Novak <sailnfool@gmail.com>
2022-02-01 15:06:34 -0800

    Finished reconstruction of kbytes

--- scripts/func.kbytes.sh
+++ scripts/func.kbytes.sh
@@ -12,5 +12,20 @@
 ########################################################################
 if [[ -z "${__kbibyesuffix}" ]]
 then
-    export __kbibytessuffix="BKMGTPEZ"
-fi
+    export __kbibytessuffix="BEGKMPTZ"
+    export __byte=1
+    export __kibibyte=$((__byte * 1024))
+    export __kbyte=$((__byte * 1000))
+    export __mibibyte=$((__kibibyte * 1024))
+    export __mbyte=$((__kbyte * 1000))
+    export __gibibyte=$((__mibibyte * 1024))
+    export __gbyte=$((__mbyte * 1000))
+    export __tibibyte=$((__gibibyte * 1024))
+    export __tbyte=$((__gbyte * 1000))
+    export __pibibyte=$((__tibibyte * 1024))
+    export __pbyte=$((__tbyte * 1000))
+    export __eibibyte=$((__pibibyte * 1024))
+    export __ebyte=$((__pbyte * 1000))
+    export __zibibyte=$((__eibibyte * 1024))
+    export __zbyte=$((__ebyte * 1000))
+fi # if [[ -z "${__kbibyesuffix}" ]]
