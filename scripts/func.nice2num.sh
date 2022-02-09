e7d6eb724dfe319e2c1f2ad339534bf2ad78bccb
Robert E. Novak <sailnfool@gmail.com>
2022-02-08 16:11:56 -0800

    Rewrite to use arrays of values, not individual names

--- scripts/func.nice2num.sh
+++ scripts/func.nice2num.sh
@@ -19,87 +19,77 @@ then
 	export __funcnice2num=1
 
 	nice2num () {
-	if [[ $# -ne 1 ]]
-	then
-		errecho "Missing Parameter"
-		echo ""
-	else
-    ############################################################
-		# Insure the multiplier is upper case
-    ############################################################
-		nicenum=$(echo $1 | tr '[a-z]' '[A-Z]')
+    local resultnumber
+    local nicenum
+    local number
+
+	  if [[ $# -ne 1 ]]
+	  then
+		  errecho "Missing Parameter"
+		  echo ""
+	  else
+      ############################################################
+		  # Insure the multiplier is upper case
+      ############################################################
+		  nicenum=$(echo $1 | tr '[a-z]' '[A-Z]')
     
-    ############################################################
-    # Strip the number off of the front
-    ############################################################
-		number=$(echo $nicenum | sed 's/^[^0-9]*\([0-9][0-9]*\).*$/\1/')
+      ############################################################
+      # Strip the number off of the front
+      ############################################################
+		  number=$(echo $nicenum | sed 's/^[^0-9]*\([0-9][0-9]*\).*$/\1/')
 
-    ############################################################
-    # Now strip the bibyte suffix off the end
-    ############################################################
-		multiplier=$(echo $nicenum | \
-			sed "s/^.*[0-9]*\([${__kbibytessuffix}]\)/\1/")
+      ############################################################
+      # Now strip the bibyte suffix off the end
+      ############################################################
+		  multiplier=$(echo $nicenum | \
+			  sed "s/^.*[0-9]*\([${__kbibytessuffix}]\)/\1/")
 
-		if [[ -z "${multiplier}" ]]
-		then
-			echo ${number}
-		else
-			case $multiplier in
-			B)
-        resultnumber=$(echo $number * $__byte) | bc)
-				;;
-			KIB)
-        resultnumber=$(echo $number * $__kibibyte) | bc)
-				;;
-			K)
-        resultnumber=$(echo $number * $__kbyte) | bc)
-				;;
-			MIB)
-        resultnumber=$(echo $number * $__mibibyte) | bc)
-				;;
-			M)
-        resultnumber=$(echo $number * $__mbyte) | bc)
-				;;
-			GIB)
-        resultnumber=$(echo $number * $__gibibyte) | bc)
-				;;
-			G)
-        resultnumber=$(echo $number * $__gbyte) | bc)
-				;;
-			TIB)
-        resultnumber=$(echo $number * $__tibibyte) | bc)
-				;;
-			T)
-        resultnumber=$(echo $number * $__tbyte) | bc)
-				;;
-			PIB)
-        resultnumber=$(echo $number * $__pibibyte) | bc)
-				;;
-			P)
-        resultnumber=$(echo $number * $__pbyte) | bc)
-				;;
-			EIB)
-        resultnumber=$(echo $number * $__eibibyte) | bc)
-				;;
-			E)
-        resultnumber=$(echo $number * $__ebyte) | bc)
-				;;
-			ZIB)
-        resultnumber=$(echo $number * $__zibibyte) | bc)
-				;;
-			Z)
-        resultnumber=$(echo $number * $__zbyte) | bc)
-				;;
-			\?)
-				errecho "Bad Suffix $multiplier"
-				;;
-			esac
+		  if [[ -z "${multiplier}" ]]
+		  then
+			  echo ${number}
+		  else
+        if [[ "${#multiplier}" -eq 1 ]]
+        then
+
+          ##################################################
+          # To understand the following existence test, see:
+          # https://linuxhint.com/associative_arrays_bash_examples/
+          # Also see:
+          # https://wiki.bash-hackers.org/syntax/pe#use_an_alternate_value
+          ##################################################
+          if [[ ! ${__kbytesvalue[${multiplier}]+_} ]]
+          then
+            errecho -i "multiplier \"${multiplier}\" not found"
+            errecho -i "Please use a nice number suffix in the"
+            errecho -i "following set for decimal powers"
+            errecho -i "${__kbytessuffix}"
+            exit 1
+          fi
+          resultnumber=${__kbytesvalue[${multiplier}]}
+        else
+
+          ##################################################
+          # To understand the following existence test, see:
+          # https://linuxhint.com/associative_arrays_bash_examples/
+          # Also see:
+          # https://wiki.bash-hackers.org/syntax/pe#use_an_alternate_value
+          ##################################################
+          if [[ ! ${__kbibytesvalue[${multiplier}]+_} ]]
+          then
+            errecho -i "multiplier \"${multiplier}\" not found"
+            errecho -i "Please use a nice number suffix in the"
+            errecho -i "following set for decimal powers"
+            errecho -i "${!__kbibytessuffix[@]}"
+            exit 1
+          fi
+          resultnumber=${__kbibytesvalue[${multiplier}]}
+        fi
+      fi
 			echo $resultnumber
 		fi
-	fi
-	##########
-	# End function nice2num
-	##########
+	  ##########
+	  # End function nice2num
+	  ##########
 	}
 	export -f nice2num
 fi # if [[ -z "${__funcnice2num}" ]]
