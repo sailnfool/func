224102a121a227c0bbe9b673242a8ed0a591aeac
Robert E. Novak <sailnfool@gmail.com>
2022-03-20 17:26:09 -0700

    Added documentation header, cleaned up comments

--- tests/dotests.sh
+++ tests/dotests.sh
@@ -4,11 +4,24 @@ scriptname=${0##*/}
 # Copyright (C) 2022 Sea2cloud Storage, Inc.  All Rights Reserved
 # Modesto, CA 95356
 #
+# Author - Robert E. Novak aka REN
+# sailnfool@gmail.com
+# skype: sailnfool.ren
+#
+# License CC by Sea2Cloud Storage, Inc.
+# see https://creativecommons.org/licenses/by/4.0/legalcode
+# for a complete copy of the Creative Commons Attribution license
+#_____________________________________________________________________
+# Rev.|Auth.| Date     | Notes
+#_____________________________________________________________________
+# 1.0 | REN |02/20/2022| Initial Release
+#_____________________________________________________________________
+#
 # dotests - run a series of scripts to test the bash functions in
 #           the tests subdirectory which should have a 1-1
 #           correspondence to the bash functions in the scripts
 #           directory.  Note that each of the test scripts must contain
-#           the "nametext" string to be able to announce the teste
+#           the "nametext" string to be able to announce the test
 #           which is being run.  The test functions exit with either
 #           an exit value of 1 <FAIL> or 0 <PASS>
 #
@@ -18,21 +31,23 @@ scriptname=${0##*/}
 #           which will take a bit of work to not only add it but to
 #           verify that it is supported in a testing script and not
 #           produce excess output if it does not.
-# Author - Robert E. Novak aka REN
-# sailnfool@gmail.com
-# skype: sailnfool.ren
 ########################################################################
 
 fail=0
 ########################################################################
-# COlor sequences cribbed from:
+# Color sequences cribbed from:
 # https://en.wikipedia.org/wiki/ANSI_escape_code
 ########################################################################
 failstring="[\033[91mFAIL\033[m]"
 passstring="[\033[92mPASS\033[m]"
 nametext="TESTNAME="
-for test_script in tester.*.sh
+testprefix="tester"
+for test_script in ${testprefix}.*.sh
 do
+  ######################################################################
+  # pull the TESTNAME from each testing file and issue an error if it
+  # does not exist
+  ######################################################################
   testname=$(grep -h "${nametext}" ${test_script})
   if [[ -z "${testname}" ]]
   then
@@ -40,6 +55,11 @@ do
     echo "${failstring} ${test_script}"
     ((fail++))
   fi
+
+  ######################################################################
+  # Execute each testscript and isse the pass/fail message as
+  # appropriate
+  ######################################################################
   if [[ ! ${test_script} ]]
   then
     echo -e "${failstring} ${test_script}\n\t${testname:${#nametext}}"
@@ -48,4 +68,9 @@ do
     echo -e "${passstring} ${test_script}\n\t${testname:${#nametext}}"
   fi
 done
+
+########################################################################
+# Note that we have kept track if any tests failed.  If all worked,
+# then we exit cleanly.
+########################################################################
 exit ${fail}
