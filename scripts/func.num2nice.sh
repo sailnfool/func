65a881cc7acb43b0363469752dbfe8e0b13a65f9
Robert E. Novak <sailnfool@gmail.com>
2022-03-26 20:11:29 -0700

    commented out diagnostics.  Still a problem...
    when kindex=0... should not happen

--- scripts/func.num2nice.sh
+++ scripts/func.num2nice.sh
@@ -28,9 +28,16 @@ then
 	export __funcnum2nice=1
 
 	num2nice () {
-    local resultnumber
-    local nicenum
-    local number
+    local convert
+    local bignumber
+    local kdivisor
+    local kbibquotient
+    local kquotient
+    local kprefix
+    local kbibprefix
+    local foundk
+    local foundkbib
+
 
     ####################################################################
     # There must be at least one argument, the number to be converted
@@ -56,21 +63,44 @@ then
       else
         convert="kbyte"
         bignumber=$1
-      fi #      if [[ "$1" = "-b" ]]
+    fi #      if [[ "$1" = "-b" ]]
     fi #	  if [[ $# -lt 1 ]]
 
+    ####################################################################
+    # The idea here is that we take the big number and divide it by the
+    # valus of the various named powers (BYT KiB, ..) and if the result
+    # is less than 1000 (or 1024) we have the prefix for the named
+    # power.
+    ####################################################################
     for (( i=0; i < ${#__kbytessuffix}; i++))
     do
+#      echo "__kbytessuffix:$i:1 = ${__kbytessuffix:$i:1}"
+#      echo "__kbytesvalue[${__kbytessuffix:$i:1}] = "\
+#        ${__kbytesvalue[${__kbytessuffix:$i:1}]}
+
+#      echo "__kbibytessufix[$i] = ${__kbibytessuffix[$i]}"
+#      echo "__kbibytesvalue[${__kbibytessuffix[$i]}] = "\
+#          ${__kbibytesvalue[${__kbibytessuffix[$i]}]}
+
       kdivisor=${__kbytesvalue[${__kbytessuffix:$i:1}]}
       kbibdivisor=${__kbibytesvalue[${__kbibytessuffix[$i]}]}
-      kquotient=$((bignumber/kdivisor))
-      kbibquotient=$((bignumber/kbibdivisor))
+
+#      echo "kdivisor = ${kdivisor}"
+#      echo kbibdivisor = "${kbibdivisor}"
+
+      kquotient=$(echo "${bignumber} / ${kdivisor}" | bc)
+      kbibquotient=$(echo "${bignumber} / ${kbibdivisor}" | bc)
+
+#      echo "kquotient = ${kquotient}"
+#      echo "kbibquotient = ${kbibquotient}"
+
       if [[ "${kquotient}" -lt "1000" ]]
       then
         kprefix=${kquotient}
         foundk="TRUE"
         if [[ "${convert}" = "kbyte" ]]
         then
+          kindex=$i
           break
         fi
       fi
@@ -80,18 +110,27 @@ then
         foundkbib="TRUE"
         if [[ "${convert}" = "kbibyte" ]]
         then
+          kindex=$i
           break
         fi
       fi
     done
     if [[ ( "${convert}" = "kbyte" ) && ( "${foundk}" = "TRUE" ) ]]
     then
-      echo "${kprefix}${__kbytessuffix[$i]}"
-      exit 0
+      result=$(echo "${kprefix}${__kbytessuffix:${kindex}:1}")
+      if [[ "${#result}" -le 4 ]]
+      then
+        echo ${result}
+        exit 0
+      else
+        echo "Invalid result, kindex=${kindex}, result=${result}, "\
+          "kprefix=${kprefix}"
+        exit 1
+      fi
     fi
     if [[ ( "${convert}" = "kbibyte" ) && ( "${foundkbib} = ""TRUE" ) ]]
     then
-      echo "${kbibprefix}${__kbibytessuffix[$i]}"
+      echo "${kbibprefix}${__kbibytessuffix[${kindex}]}"
       exit 0
     fi
     exit 1
