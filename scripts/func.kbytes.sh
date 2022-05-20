979d5216bf10d753387e594cfb78c07010d1d824
Robert E. Novak <sailnfool@gmail.com>
2022-05-19 17:30:52 -0700

    Took re_nicenumber out of func.regex and ...
    placed it here.

--- scripts/func.kbytes.sh
+++ scripts/func.kbytes.sh
@@ -6,6 +6,8 @@
 #_____________________________________________________________________
 # Rev.|Auth.| Date     | Notes
 #_____________________________________________________________________
+# 1.2 | REN |05/19/2022| moved re_nicenumber to here instead of
+#                      | func.regex
 # 1.1 | REN |02/08/2022| redo using arrays instead of named variables
 #                      | see the accompanying test function
 # 1.0 | REN |02/01/2022| Reconstructed from func.nice2num
@@ -36,4 +38,56 @@ then
 #    echo "__kbibytesvalue = ${__kbibytesvalue[@]}"
     export __kbytesvalue
     export __kbibytesvalue
+	  ####################################################################
+	  # Create a regular expression for nicenumbers
+	  ####################################################################
+    let1=/tmp/letters1$$
+    let2=/tmp/letters2$$
+    sorted=/tmp/sorted$$
+    rm -f ${let1} ${let2} ${sorted}
+  
+	  ####################################################################
+	  # First we sort through all of the letters in the kbibytessuffix
+	  # and kbytessuffix to put them in a single string.
+	  ####################################################################
+	  allcat="${__kbibytessuffix[*]} ${__kbytessuffix}"
+   
+	  for i in $(seq 0 ${#allcat})
+	  do
+	    echo -e "${allcat:${i}:1}\n" >> ${let1}
+	  done
+
+	  ####################################################################
+    # Make a second lower case only copy of the strings
+	  ####################################################################
+    tr "A-Z" "a-z" < ${let1} > ${let2}
+
+	  ####################################################################
+    # Make a sorted, unique set of the strings and delete empty lines
+	  ####################################################################
+	  cat ${let1} ${let2} | sort -u | \
+      tr -d " " | sed -e '/^$/d' > ${sorted}
+
+	  ####################################################################
+    # Make a single string of all of the letters in the nicenumber
+    # names so we will only recognize a suffix containing those letters
+    # (this is not perfect and could be improved upon)
+	  ####################################################################
+	  while read inletter
+	  do
+	    matcher="${matcher}${inletter}"
+	  done < ${sorted}
+
+	  ####################################################################
+    # Create the regular expression that will recognize strings like:
+    # 1M, 1MiB, 10T, 10TiB, etc.
+	  ####################################################################
+	  re_nicenumber="[0-9][0-9]*[${matcher}][${matcher}]*"
+
+	  ####################################################################
+    # Delete the temporary files.
+	  ####################################################################
+    rm -f ${let1} ${let2} ${sorted}
+    export re_nicenumber
+
 fi # if [[ -z "${__kbytessuffix}" ]]
