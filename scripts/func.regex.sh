5e18924bd2d82bc6b9f34fc1e95d9c5312871046
Robert E. Novak <sailnfool@gmail.com>
2022-05-27 12:35:31 -0700

    Forgot to export new entries.  Made them all the
    same

--- scripts/func.regex.sh
+++ scripts/func.regex.sh
@@ -46,16 +46,12 @@ then
   #
   # Note that there is also a "re_nicenumber" found in func.kbytes
   ######################################################################
-  re_hexdigit='^[0-9a-fA-F]$'
-	re_hexnumber='^[0-9a-fA-F][0-9a-fA-F]*$'
-  re_digit='^[0-9]$'
-	re_integer='^[0-9][0-9]*$'
-	re_signedinteger='^[+\-][0-9][0-9]*$'
-	re_decimal='^[+\-][0-9]*\.[0-9]*$'
-  re_cryptohash='^[0-9a-fA-F]{3}:[0-9a-fA-F][0-9a-fA-F]*$'
-  export re_hexnumber
-  export re_integer
-  export re_signedinteger
-  export re_decimal
+  export re_hexdigit='^[0-9a-fA-F]$'
+  export re_hexnumber='^[0-9a-fA-F][0-9a-fA-F]*$'
+  export re_digit='^[0-9]$'
+  export re_integer='^[0-9][0-9]*$'
+  export re_signedinteger='^[+\-][0-9][0-9]*$'
+  export re_decimal='^[+\-][0-9]*\.[0-9]*$'
+  export re_cryptohash='^[0-9a-fA-F]{3}:[0-9a-fA-F][0-9a-fA-F]*$'
 
 fi # if [[ -z "${__func_regex}" ]]
