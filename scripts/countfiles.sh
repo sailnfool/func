c245ac43802b8fedcad2e040e6785000fc06ace0
Robert E. Novak <sailnfool@gmail.com>
2022-07-18 13:26:57 -0700

    Tweaked for Linuxhint blog

--- scripts/countfiles.sh
+++ scripts/countfiles.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+scriptname=${0##*/}
 ########################################################################
 # Author: Robert E. Novak
 # email: sailnfool@gmail.com
@@ -10,27 +11,53 @@
 #_____________________________________________________________________
 # Rev.|Aut| Date     | Notes
 #_____________________________________________________________________
+# 1.1 |REN|07/18/2022| Added -d and -v command line options
 # 1.0 |REN|05/16/2022| Initial Release
 #_____________________________________________________________________
 #
 source func.debug
 source func.errecho
 source func.insufficient
+source func.regex
 
 NUMARGS=1
+verbosemode="FALSE"
+verboseflag=""
+FUNC_DEBUG=DEBUGOFF
 
-USAGE="\r\n${0##*/} [-h] <dir>\r\n
-\t\tSummarize the count of the number of files in this tree\r\n
-\t-h\t\tPrint this message\r\n
+USAGE="\n${0##*/} [-hv] [-d <#>] <dir>\n
+\t\tSummarize the count of the number of files in this tree\n
+\t-d\t<#>\tSet the diagnostic levels\n
+\t\t\tUse -vh to see debug modes\n
+\t-h\t\tPrint this message\n
+\t-v\t\tTurn on verbose mode\n
 "
-optionargs="h"
+optionargs="d:hv"
 while getopts ${optionargs} name
 do
   case ${name} in
+	d)
+    if [[ ! "${OPTARG}" =~ $re_digit ]]
+    then
+      errecho "${0##/*}" "${LINENO}" "-d requires a decimal digit"
+      errecho -e "${USAGE}"
+      errecho -e "${DEBUG_USAGE}"
+      exit 1
+    fi
+		FUNC_DEBUG="${OPTARG}"
+		export FUNC_DEBUG
+		if [[ $FUNC_DEBUG -ge 9 ]]
+		then
+			set -x
+		fi
+		;;
   h)
     errecho -e ${USAGE}
     exit 0
     ;;
+  v)
+    verbosemode="TRUE"
+    verboseflag="-v"
   \?)
     errecho "-e" "invalid option: -${OPTARG}"
     errecho "-e" ${USAGE}
