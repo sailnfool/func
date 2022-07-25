0e9602e667b80b9df7b2b93384de8b98b710e5f0
Robert E. Novak <sailnfool@gmail.com>
2022-07-24 23:06:55 -0700

    Cleaned up documentation

--- tests/tester.func.dec2hex.sh
+++ tests/tester.func.dec2hex.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
-####################
+########################################################################
+# Copyright (C) 2022 Robert E. Novak  All Rights Reserved
+# Modesto, CA 95356
+########################################################################
+#
+# tester func.dec2hex - Testing conversion of decimal numbers to
+#                       hexadecimal numbers.
+#
 # Author - Robert E. Novak aka REN
 #	sailnfool@gmail.com
-#	skype:sailnfool.ren
+# License CC by Sea2Cloud Storage, Inc.
+# see https://creativecommons.org/licenses/by/4.0/legalcode
+# for a complete copy of the Creative Commons Attribution license
 #_____________________________________________________________________
 # Rev.|Aut| Date     | Notes
 #_____________________________________________________________________
 # 1.0 |REN|03/20/2022| testing dec2hex
 #_____________________________________________________________________
-#
-########################################################################
+
 source func.errecho
 source func.dec2hex
 
 TESTNAME="Test of function global (func.dec2hex) from\n\thttps://github.com/sailnfool/func"
-USAGE="\r\n${0##*/} [-[hv]]\r\n
-\t-h\t\tPrint this message\r\n
-\t-v\t\tVerbose mode to show values\r\n
+USAGE="\n${0##*/} [-[hv]]\n
+\t-h\t\tPrint this message\n
+\t-v\t\tVerbose mode to show values\n
 \t\tVerifies that the function will convert decimal numbers to zero
 \t\tprefixed hexadecimal values
-\t\tNormally emits only PASS|FAIL message\r\n
+\t\tNormally emits only PASS|FAIL message\n
 "
 
 optionargs="hv"
-verbose_mode="FALSE"
+verbosemode="FALSE"
+verboseflag=""
 
 while getopts ${optionargs} name
 do
@@ -33,10 +42,11 @@ do
 		exit 0
 		;;
 	v)
-		verbose_mode="TRUE"
+		verbosemode="TRUE"
+    verboseflag="-v"
 		;;
 	\?)
-		errecho "-e" "invalid option: -$OPTARG"
+		errecho "-e" "invalid option: -${OPTARG}"
 		errecho "-e" ${USAGE}
 		exit 1
 		;;
@@ -63,16 +73,15 @@ tr[1]="00f"
 tv[2]=255
 td[2]=3
 tr[2]="0ff"
-maxtests=2
 
 fail=0
 
 ########################################################################
 # ti short for testindex
 ########################################################################
-for ti in $(seq 0 ${maxtests})
+for ti in $(seq 0 ${#tv[@]})
 do
-  if [[ "${verbose_mode}" == "TRUE" ]]
+  if [[ "${verbosemode}" == "TRUE" ]]
   then
     echo "$(func_dec2hex ${tv[${ti}]} ${td[${ti}]}) should ${tr[${ti}]}"
   fi
