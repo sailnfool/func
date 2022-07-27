98f7c15b00d6953a54914d42d56642165831a614
Robert E. Novak <sailnfool@gmail.com>
2022-07-26 20:51:40 -0700

    Load the associative arrays from ...
    files stored on this machine

--- scripts/hash.loadcanonical.sh
+++ scripts/hash.loadcanonical.sh
@@ -40,109 +40,47 @@ then
     if [[ "$#" -eq 1 ]] && [[ "$1" == "-v" ]]
     then
       verbosemode="TRUE"
+      verboseflag=""
     fi
-    wavfuncentry
-    if [[ "${FUNC_DEBUG}" -ge "${DEBUGWAVAR}" ]]
-    then
-    ######################################################################
+    wavfuncentry ${verboseflag}
+    ####################################################################
     # Now read the canonical files to load up the canonical hash info
     # Note that this is subtle the way we generate the list of
     # filenames and the list of Associative Arrays
     # A similar clever trick could eliminate the case statement, 
     # but my # brain is aching from the number of levels of
     # indirection.
-    ######################################################################
-    unset Cnum2hash
-    unset Cnum2bin
-    unset Cnum2bits
-    unset Cnum2hexdigits
-    unset Chash2num
-# 	  declare -A Cnum2hash
-# 	  declare -A Cnum2bin
-# 	  declare -A Cnum2bits
-# 	  declare -A Cnum2hexdigits
-# 	  declare -A Chash2num
-# 	  export Cnum2hash
-# 	  export Cnum2bin
-# 	  export Cnum2bits
-# 	  export Cnum2hexdigits
-# 	  export Chash2num
+    ####################################################################
+#     unset Cnum2hash
+#     unset Cnum2bin
+#     unset Cnum2bits
+#     unset Cnum2hexdigits
+#     unset Chash2num
 
     for filesuffix in num2hash num2bin num2bits num2hexdigits hash2num
     do
+      local -n newarr=C${filesuffix}
+      local arrname
+      arrname=$(echo C${filesuffix})
       filename=${YesFSdiretc}/$(eval echo \$F${filesuffix})
-      waverrindentvar "filename=${filename}"
+      waverrindentvar ${verboseflag} "filename=${filename}"
       while read key value
       do
-        waverrindentvar "key='${key}', value='${value}'"
-        eval \$C${filesuffix}+=("[${key}"]="${value}")
-        waverrindentvar $(eval "\$C${filesuffix}["${key}"]")
-#         case ${filesuffix} in
-#           num2hash)
-#             Cnum2hash+=(["${key}"]="${value}")
-#             waverrindentvar "${Cnum2hash["${key}"]}"
-#             ;;
-#           num2bin)
-#             Cnum2bin+=(["${key}"]="${value}")
-#             if [[ ! "$(which ${Cnum2hash["${key}"]})" == "${value}" ]]
-#             then
-#               stderrecho "Binary for '${Cnum2hash["${key}"]}' " \
-#                 "not found at path '${value}'"
-#             fi
-#             waverrindentvar "${Cnum2bin["${key}"]}"
-#             ;;
-#           num2bits)
-#             Cnum2bits+=(["${key}"]="${value}")
-#             waverrindentvar "${Cnum2bits["${key}"]}"
-#             ;;
-#           num2hexdigits)
-#             Cnum2hexdigits+=(["${key}"]="${value}")
-#             waverrindentvar "${Cnum2hexdigits["${key}"]}"
-#             ;;
-#           hash2num)
-#             Chash2num+=(["${key}"]="${value}")
-#             waverrindentvar "${Chash2num["${key}"]}"
-#             ;;
-#           \?)
-#             stderrecho "${0##*/}:${FUNCNAME}:${LINENO}: " \
-#               "this should never happen"
-#             exit 1
-#             ;;
-#         esac
+#         waverrindentvar ${verboseflag} "key='${key}', value='${value}'"
+#         waverrindentvar ${verboseflag} "${arrname} ["${key}"] '${value}':: "
+#         $(eval C${filesuffix}+=\([\"${key}\"]\="${value}"\))
+         newarr+=(["${key}"]="${value}")
+         waverrindentvar ${verboseflag} "${arrname}["${key}"]=${newarr["${key}"]}"
       done < ${filename}
 
-      for key in "$(eval \$!{C${filesuffix}[@]})"
+      for nkey in "${!newarr[@]}"
       do
-        waverrindentvar "C${filesuffix}["${key}"]='$(eval \${C${filesuffix}["${key}"]})'"
+        waverrindentvar ${verboseflag} "${arrname}["${nkey}"]=${newarr["${nkey}"]}"
       done
 
     done # for filesuffix in num2hash num2bin num2bits num2hexdigits hash2num
 
-    ####################################################################
-    # Verify that the associative arrays are initialized if
-    # FUNC_DEBUG >= ${DEBUGWAVAR}
-    ####################################################################
-#     for key in "$!{Cnum2hash[@]}"
-#     do
-#       waverrindentvar "Cnum2hash["${key}"]='${Cnum2hash["${key}"]}'"
-#     done
-#     for key in "${Cnum2bin[@]}"
-#     do
-#       waverrindentvar "Cnum2bin["${key}"]='${Cnum2bin["${key}"]}'"
-#     done
-#     for key in "${Cnum2bits[@]}"
-#     do
-#       waverrindentvar "Cnum2bits["${key}"]='${Cnum2bits["${key}"]}'"
-#     done
-#     for key in "${Cnum2hexdigits[@]}"
-#     do
-#       waverrindentvar "Cnum2hexdigits["${key}"]='${Cnum2hexdigits["${key}"]}'"
-#     done
-#     for key in "${Chash2num[@]}"
-#     do
-#       waverrindentvar "hash2num["${key}"]='${hash2num["${key}"]}'"
-#     done
-    wavfuncexit
+    wavfuncexit ${verboseflag}
   }
   export hash_hashloadcanonical
 fi # if [[ -z "${__loadcanonical}" ]]
