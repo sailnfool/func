bcbb12e3381a25071a3e176d9165123a13c0ddc2
Robert E. Novak <sailnfool@gmail.com>
2022-07-29 18:37:57 -0700

    Cleaned up documentation

--- scripts/func.toseconds.sh
+++ scripts/func.toseconds.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
+########################################################################
+# Copyright (C) 2022 Robert E. Novak  All Rights Reserved
+# Modesto, CA 95356
+########################################################################
 #######################################################################
+#
+# toseconds - convert a time to number of seconds.  Has to be
+#             flexible for HH:MM:SS or MM:SS or just SS.xx
+#
 # Author: Robert E. Novak
 # email: sailnfool@gmail.com
-# Copyright (C)2022 Sea2Cloud Storage, Inc.  All Rights Reserved
-# Modesto, CA 95356
-#
-# convert a time to number of seconds
-# Has to be flexible for HH:MM:SS or MM:SS or just SS.xx
-#
-#######################################################################
-# Rev.|Aut| Date     | Notes
 #_____________________________________________________________________
+# Rev.|Aut| Date     | Notes
 # 1.0 |REN|02/01/2022| Initial Release
 #_____________________________________________________________________
-#######################################################################
 
 if [[ -z "${__functoseconds}" ]]
 then
@@ -23,11 +23,10 @@ then
 
   function toseconds() {
 
-    local secondresult
+    local -a secondresult
     local tresult
     local result
 
-    declare -a secondresult
     if [[ $# -lt 1 ]]
     then
       insufficient 1 "at least one argument in time format required"
@@ -36,6 +35,7 @@ then
     secondresult[0]=$(echo "$1" | awk -F: '{ if (NF == 1) {print $NF} }' | bc)
     secondresult[1]=$(echo "$1" | awk -F: '{ if (NF == 2) {print $1 "* 60 + " $2} }' | bc)
     secondresult[2]=$(echo "$1" | awk -F: '{ if (NF == 3) {print $1 " * 3600 + " $2 " * 60 + " $3} }' | bc)
+    
     for i in $(seq 0 2)
     do
       tresult="${secondresult[${i}]}"
@@ -57,4 +57,3 @@ then
    }
 	export -f toseconds
 fi # if [[ -z "${__functoseconds}" ]]
-
