6e2b61b0556825f0fe914fe9c27725557c4165fd
Robert E. Novak <sailnfool@gmail.com>
2022-07-29 18:37:10 -0700

    Cleaned up documentation and ...
    added error checking.

--- scripts/func.random.sh
+++ scripts/func.random.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+########################################################################
+# Copyright (C) 2022 Robert E. Novak  All Rights Reserved
+# Modesto, CA 95356
+########################################################################
+#
+# func_intrandom - return a random positive integer from /dev/random
+#
+# func_intrandomrange - return a number in the inerval [0-#)
+#
+# Author - Robert E. Novak aka REN
+#	sailnfool@gmail.com
+# License CC by Sea2Cloud Storage, Inc.
+# see https://creativecommons.org/licenses/by/4.0/legalcode
+# for a complete copy of the Creative Commons Attribution license
+#_____________________________________________________________________
+# Rev.|Aut| Date     | Notes
+#_____________________________________________________________________
+# 1.0 |REN|07/29/2022| Initial Release
+#_____________________________________________________________________
+
+########################################################################
+# func_intrandom - return a random positive integer from /dev/random
+########################################################################
 if [[ -z "$__funcintrandom" ]]
 then
 	export __funcintrandom=1
@@ -13,12 +36,34 @@ then
 	}
 	export -f func_intrandom
 	
+########################################################################
+# func_intrandomrange - return a number in the inerval [0-#)
+# The function func_intrandom returns a 4 byte positive integer
+# which is guaranteed to work on 64-bit machines.
+########################################################################
+
 	function func_intrandomrange() {
 
-    local num
+    source func.regex
 
-		num=$(func_intrandom)
-		echo $(expr $num % $1)
+    local maxsignedint=$(echo "2^63-1" | bc)
+    local checksign
+
+    if [[ ! "${1}" =~ $re_decimal ]]
+    then
+      errecho "Argument is not a decimal number"
+      exit 1
+    else
+      checksign=$(echo "${1} - ${maxsignedint}" | bc)
+      if [[ "${checksign:0:1}" == "-" ]]
+      then
+        errecho "Argument out of range ${1}"
+        errecho "Argument greater than maximum signed integer"
+        errecho "${maxsignedint}"
+        exit 1
+      fi
+    fi
+    echo $(( $(func_intrandom) % $1 ))
 	}
 	export -f func_intrandomrange
 fi # if [[ -z "$__funcintrandom" ]]
