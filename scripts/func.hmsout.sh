c80988dd8e48dcc0a427d73767fb1c47e8912eab
Robert E. Novak <sailnfool@gmail.com>
2022-07-29 18:31:50 -0700

    Cleaned up documentation ...
    Added conversion of > 24 hours to days.  This
    may need to be controlled by an option parameter.

--- scripts/func.hmsout.sh
+++ scripts/func.hmsout.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-####################
+########################################################################
+# Copyright (C) 2020 Robert E. Novak  All Rights Reserved
+# Modesto, CA 95356
+########################################################################
+#
 # hmsout <number> <units>
-#   Convert the number in hours, minutes or seconds into H:m:s format
-####################
+#        Convert the number in hours, minutes or seconds
+#        into H:m:s format
+#
 # Author - Robert E. Novak aka REN
 #	sailnfool@gmail.com
-#	skype:sailnfool.ren
 #_____________________________________________________________________
 # Rev.|Aut| Date     | Notes
 #_____________________________________________________________________
+# 1.1 |REN|07/29/2022| Added conversion of > 24 hours to days.  It may
+#                    | break some old usage
 # 1.0 |REN|01/15/2020| original version
 #_____________________________________________________________________
-####################
+
 source func.errecho
+
 if [ -z "${__func_hmsout}" ]
 then
 	export __func_hmsout=1
@@ -47,7 +54,12 @@ then
         exit 1
          ;;
     esac
-    printf "%02d:%02d:%02d" "${hour}" "${mins}" "${sec}"
+    if [[ "${hour}" -ge "24" ]]
+    then
+      printf "$d days" $((days / 24))
+      ((hour=hour%24))
+    fi
+    printf "%02d:%02d:%02d\n" "${hour}" "${mins}" "${sec}"
   }
 fi # if [ -z "${__func_hmsout}" ]
 # vim: set syntax=bash, lines=55, columns=78,colorcolumn=72
