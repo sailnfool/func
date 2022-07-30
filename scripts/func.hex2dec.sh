b3192aae941710a4d162c5b66fdb4bae2933efa5
Robert E. Novak <sailnfool@gmail.com>
2022-07-29 18:30:56 -0700

    Cleaned up documentation

--- scripts/func.hex2dec.sh
+++ scripts/func.hex2dec.sh
@@ -2,11 +2,12 @@
 ########################################################################
 # Copyright (C) 2022 Sea2cloud Storage, Inc.  All Rights Reserved
 # Modesto, CA 95356
+########################################################################
 #
 # func_hex2dec Convert the argument from Hex to decimal
+#
 # Author - Robert E. Novak aka REN
 # sailnfool@gmail.com
-# skype:sailnfool.ren
 # License CC by Sea2Cloud Storage, Inc.
 # see https://creativecommons.org/licenses/by/4.0/legalcode
 # for a complete copy of the Creative Commons Attribution license
@@ -16,8 +17,7 @@
 #_____________________________________________________________________
 # 1.0 |REN|05/27/2022| Initial Release
 #_____________________________________________________________________
-#
-########################################################################
+
 if [[ -z "${__funchex2dec}" ]]
 then
   export __funchex2dec=1
