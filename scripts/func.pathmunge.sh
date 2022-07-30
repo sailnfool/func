ab44b96e27d9477f9033a9df092ef6f880e70e10
Robert E. Novak <sailnfool@gmail.com>
2022-07-29 18:36:34 -0700

    Cleaned up documentation and ...
    removed vim directive

--- scripts/func.pathmunge.sh
+++ scripts/func.pathmunge.sh
@@ -6,6 +6,14 @@
 #
 # pathmunge - add a directory to $PATH
 #
+# The directory <dir> is added before the rest of the directories in
+# PATH.  The optional argument "after" places the directory after all
+# other directories in PATH.  This script guarantees that links or
+# symbolic links are decoded via "realpath(1)" and that the specified
+# directory is only placed in PATH one time.  The output of the
+# pathmunge replaces the contents of $HOME/.bashrc.addpath which
+# is sourced as the last line of $HOME/.bashrc
+#
 # This script evolved from stackoverflow
 # https://stackoverflow.com/questions/5012958 \
 # /what-is-the-advantage-of-pathmunge-over-grep
@@ -20,21 +28,15 @@
 #_____________________________________________________________________
 # Rev.|Aut| Date     | Notes
 #_____________________________________________________________________
+# 2.1 |REN|07/29/2022| removed vim directive
 # 2.0 |REN|11/13/2019| added vim directive and header file
 # 1.0 |REN|09/06/2018| original version
 #_____________________________________________________________________
-########################################################################
-# The directory <dir> is added before the rest of the directories in
-# PATH.  The optional argument "after" places the directory after all
-# other directories in PATH.  This script guarantees that links or
-# symbolic links are decoded via "realpath(1)" and that the specified
-# directory is only placed in PATH one time.  The output of the
-# pathmunge replaces the contents of $HOME/.bashrc.addpath which
-# is sourced as the last line of $HOME/.bashrc
-########################################################################
+
 if [[ -z "${__funcpathmunge}" ]]
 then
 	export __funcpathmunge=1
+
   source func.errecho
 
 	function func_pathmunge() {
@@ -68,4 +70,3 @@ then
   ###################################################################### 
 	export -f func_pathmunge
 fi # if [[ -z "${__funcpathmunge}" ]]
-# vim: set syntax=bash, lines=999, columns=78,colorcolumn=72
