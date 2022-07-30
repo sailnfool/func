b638ac2acf39770713d5def1a505ac85932bc8c3
Robert E. Novak <sailnfool@gmail.com>
2022-07-29 18:37:34 -0700

    Cleaned up documentation

--- scripts/func.regex.sh
+++ scripts/func.regex.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 ########################################################################
+# Copyright (C) 2022 Robert E. Novak  All Rights Reserved
+# Modesto, CA 95356
+########################################################################
+#
+# regex - Define a set of regular expressions for various tests 
+#         of inputs.  When you want to test a variable, do NOT
+#         place the expansion of the re_xxxx value in quotes.
+#         It will not work.
+#
 # Author: Robert E. Novak
 # email: sailnfool@gmail.com
-# Copyright (C) 2022 Sea2Cloud Storage, Inc. All Rights Reserved
-# Modesto, CA 95356
-#
-# Define a set of regular expressions for various tests of inputs
-# When you want to test a variable, do NOT place the expansion of
-# the re_xxxx value in quotes.  It will not work.
-#
-########################################################################
 #_____________________________________________________________________
 # Rev.|Aut| Date     | Notes
 #_____________________________________________________________________
 # 1.4 |REN|05/27/2022| Added re_hexdigit, re_digit
 # 1.3 |REN|05/19/2022| Added re_cryptohash, moved re_nicenumber to
-#                      | func.kbytes
+#                    | func.kbytes
 # 1.2 |REN|02/20/2022| Modernized to [[]]
 # 1.1 |REN|02/01/2022| Tweaked documentation
 # 1.0 |REN|03/25/2021| original version
 #_____________________________________________________________________
-#
+
 if [[ -z "${__func_regex}" ]]
 then
 	export __func_regex=1
