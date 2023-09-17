6f6c17258d5d7c448e297f61369d7a0ed43563b2
Robert E. Novak <sailnfool@gmail.com>
2023-09-18 07:23:41 +0800

    Cleanup and consistent variable syntax

--- scripts/func.regex.sh
+++ scripts/func.regex.sh
@@ -31,7 +31,7 @@ if [[ -z "${__func_regex}" ]] ; then
   # When you want to test a variable, do NOT place the expansion of
   # the re_xxxx value in quotes.  It will not work.
   #
-  # if [[ "$input_value" =~ $re_hexnumber ]]
+  # if [[ "$input_value" =~ ${re_hexnumber} ]]
   # then
   #     echo "$input_value is a valid hex number
   # fi
@@ -54,5 +54,9 @@ if [[ -z "${__func_regex}" ]] ; then
   export re_signedinteger='^[+\-][0-9][0-9]*$'
   export re_decimal='^[+\-][0-9]*\.[0-9]*$'
   export re_cryptohash='^[0-9a-fA-F]{3}:[0-9a-fA-F][0-9a-fA-F]*$'
+  export re_variablename='^[a-zA-Z_\][a-zA-Z_0-9]*$'
+  export re_ipv4='^[12]*[0-9]*[0-9]\.[12]*[0-9]*[0-9]\.[12]*[0-9]*[0-9]\.[12]*[0-9]*[0-9]$'
+  export re_ipv4a='^(2[1-4][0-9]|25[0-5]|1[0-9]{2}|[0-9]{1-3}).{3}(2[1-4][0-9]|25[0-5]|1[0-9]{2}|[0-9]{1-3})$'
+  export re_ipv6='^(([0-9a-fA-F]){0-4})(:([0-9a-fA-F]){0-4}){1-15}$'
 
 fi # if [[ -z "${__func_regex}" ]]
